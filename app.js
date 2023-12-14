0b8e025f7db992d9bb21d19217cb692f0bde974f
Guilherme Ferreira <guihensanfer@gmail.com>
2023-12-14 08:35:25 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const mysql = require('mysql2');
+const app = express();
+
+require('dotenv').config();
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    connectionLimit: 10,
+});
+
+app.listen(3000);
+
+
+app.get("/api/welcome", (req, res) => {
+    res.status(200).json({msg:"you are welcome!"});
+});
