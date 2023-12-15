20ec0388861fcbe012dd48bc0923afe61e931cd3
Guilherme Ferreira <guihensanfer@gmail.com>
2023-12-15 20:54:18 -0300

    Added Swagger message

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,6 +4,16 @@ router.get("/", (req, res) => {
     res.status(200).json({msg:"you are welcome1!"});
 });
 
+/**
+ * @swagger
+ * /api/welcome:
+ *   get:
+ *     summary: Retorna todos os usuários.
+ *     description: Obtém todos os usuários cadastrados.
+ *     responses:
+ *       '200':
+ *         description: Sucesso ao obter os usuários.
+ */
 router.get("/api/welcome", (req, res) => {
     res.status(200).json({msg:"you are welcome2!"});
 });
